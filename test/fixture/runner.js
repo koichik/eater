5c5d5294024c768e3743463e64389f334210a2a3
Koichi Kobayashi <koichik@improvement.jp>
2016-06-20 05:57:34 +0900

    Run subtests as separate processes

--- test/fixture/runner.js
+++ test/fixture/runner.js
@@ -1,6 +1,7 @@
 const test = require(`${process.cwd()}/lib/runner`).test;
 const assert = require('power-assert');
 
+console.log('runner', process.execArgv);
 test('assert truthy', (done) => {
   assert(true);
   done();
